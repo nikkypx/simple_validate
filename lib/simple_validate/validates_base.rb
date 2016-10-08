2e6cc33621ab6b4c9d091d9bed2916294a6c727d
David Charte <fdavidcl@protonmail.com>
2016-10-08 21:22:27 +0200

    Add condition support in base validation

--- lib/simple_validate/validates_base.rb
+++ lib/simple_validate/validates_base.rb
@@ -1,10 +1,11 @@
 module SimpleValidate
   class ValidatesBase
-    attr_accessor :message, :attribute
+    attr_accessor :message, :attribute, :condition
 
-    def initialize(attribute, message)
+    def initialize(attribute, message, condition)
       self.message   = message
       self.attribute = attribute
+      self.condition = condition
     end
   end
 end
