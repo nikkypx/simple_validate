e2ec87300443f98752c44c10579e362c9a0a1034
Nick Palaniuk <npalaniuk@gmail.com>
2016-10-20 11:52:42 -0400

    Remove unnecessary requirements

--- lib/simple_validate/validates_exclusion_of.rb
+++ lib/simple_validate/validates_exclusion_of.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 module SimpleValidate
   class ValidatesExclusionOf < ValidatesSetBase
 
--- lib/simple_validate/validates_inclusion_of.rb
+++ lib/simple_validate/validates_inclusion_of.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 module SimpleValidate
   class ValidatesInclusionOf < ValidatesSetBase
 
